b3bbec3fd763a95932d370e5ede5055346a3d8d7
velimd <you@example.com>
2018-03-05 15:26:25 +0000

    getting data and displaying into html

--- podc/blog/models.py
+++ podc/blog/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 class Article(models.Model):
 	pub_date = models.DateTimeField('date published')
-	Title = models.CharField(max_length=200)
-	Author = models.CharField(max_length=100)
-	Article = models.CharField(max_length=10000)
+	title = models.CharField(max_length=200)
+	author = models.CharField(max_length=100)
+	article_title = models.CharField(max_length=10000)
--- podc/blog/views.py
+++ podc/blog/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.template import loader
 
+from .models import Article
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_article_list = Article.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('blog/index.html')
+    context = {
+        'latest_article_list': latest_article_list,
+    }
+    return HttpResponse(template.render(context, request))
