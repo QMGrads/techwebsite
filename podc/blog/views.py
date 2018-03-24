d02e784a0bbc6632b1bae465bafb077f3d2e3613
Mustafa-1996@live.co.uk <Mustafa-1996@live.co.uk>
2018-03-24 00:22:03 +0000

    articles form and views

--- podc/blog/views.py
+++ podc/blog/views.py
@@ -3,6 +3,7 @@ from django.template import loader
 from django.shortcuts import render
 
 from .models import Article
+from .forms import PostForm
 
 def index(request):
     latest_article_list = Article.objects.order_by('-pub_date')[:5]
@@ -14,3 +15,7 @@ def index(request):
 	
 def about(request):
 	return render(request, 'blog/about.html')
+	
+def post_new(request):
+    form = PostForm()
+    return render(request, 'blog/articles.html', {'form': form})
