113f6aa6a21c1f0621916949433226149b7437e5
Mustafa-1996@live.co.uk <Mustafa-1996@live.co.uk>
2018-04-28 23:06:58 +0100

    adding user information

--- podc/blog/admin.py
+++ podc/blog/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 from .models import Article
+from .models import Users
 
 admin.site.register(Article)
+admin.site.register(Users)
--- podc/blog/models.py
+++ podc/blog/models.py
@@ -5,3 +5,7 @@ class Article(models.Model):
 	title = models.CharField(max_length=200)
 	author = models.CharField(max_length=100)
 	article_title = models.CharField(max_length=10000)
+	
+class Users(models.Model):
+	username = models.CharField(max_length=20)
+	password = models.CharField(max_length=20)
