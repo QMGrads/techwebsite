ce719b93e8163b7e3d8073d516c2d656428ff0ec
Mustafa-1996@live.co.uk <Mustafa-1996@live.co.uk>
2018-05-18 20:05:53 +0100

    Add Articles Text box and Css centre

--- podc/blog/models.py
+++ podc/blog/models.py
@@ -5,7 +5,7 @@ class Article(models.Model):
 	pub_date = models.DateTimeField(default=datetime.now, blank=True)
 	title = models.CharField(max_length=200)
 	author = models.CharField(max_length=100)
-	article_title = models.CharField(max_length=10000)
+	article_title = models.TextField()
 	
 class User(models.Model):
 	username = models.CharField(max_length=20)
